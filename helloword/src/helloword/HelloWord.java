6b0f8b527cc78ce584f603d5645b2b4b8ad1f5d5
Felipe Peronica <felipeperonica@gmail.com>
2025-04-11 11:16:18 -0300

    :tada: Commit Inicial

---
+++ helloword/src/helloword/HelloWord.java
@@ -0,0 +1,11 @@
+package helloword;
+
+public class HelloWord {
+
+	public static void main(String[] args) {
+		
+		System.out.println("Hello World!");	
+		System.out.println("A Turma Java 82 é TOP!");	
+	}
+
+}
