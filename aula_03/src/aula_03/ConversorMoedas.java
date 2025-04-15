491fb148af833c4f0eadce9640de607bc7ace059
Felipe Peronica <felipeperonica@gmail.com>
2025-04-15 11:26:41 -0300

    :sparkles: Live Code - Aula 03 - Operadores logicos, lacos condicionais e comparacao de strings

---
+++ aula_03/src/aula_03/ConversorMoedas.java
@@ -0,0 +1,54 @@
+package aula_03;
+
+import java.util.Scanner;
+
+public class ConversorMoedas {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		
+		double valorConvertido = 0.0;
+		String moeda = "";
+		
+		System.out.println("******************************");
+		System.out.println("      Conversor de moeda      ");
+		System.out.println("******************************");
+		System.out.println("                              ");
+		System.out.println("1 - Converter para Dólar (US$)");
+		System.out.println("2 - Converter para Euro  (EUR$)");
+		System.out.println("3 - Converter para Peso  (AR$)");
+		System.out.println("                              ");
+		System.out.println("******************************");
+		System.out.println("Digite a opção desejada:      ");
+		int opcao = leia.nextInt();
+		
+		System.out.println("Digite o valor em Reais (R$)");
+		double valor = leia.nextFloat();
+		
+		switch(opcao) {
+		case 1:
+			valorConvertido = valor / 5.86;	
+			moeda = "Dolar";
+			break;
+		case 2:	
+			valorConvertido = valor / 6.63;	
+			moeda = "Euro";
+			break;
+		case 3:	
+			valorConvertido = valor / 0.0049;	
+			moeda = "Peso Argentino";
+			break;
+		default: 
+			System.out.println("Opção Invalida");
+			leia.close();
+			return; //Sair
+		}
+		
+		System.out.printf("Valor convertido em %s: %.2f", moeda, valorConvertido);
+		
+		leia.close();
+		
+	}
+
+}
