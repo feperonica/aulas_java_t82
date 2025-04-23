4ea144b6cfbbe5074434a80e90ef9b49f5a24a3c
Felipe Peronica <felipeperonica@gmail.com>
2025-04-23 15:22:23 -0300

    :sparkles: Live Code - Aula 07 - Estrutura de Dados

--- aula_07/src/aula_07/Pilha.java
+++ aula_07/src/aula_07/Pilha.java
@@ -1,13 +1,18 @@
 package aula_07;
 
+import java.util.Scanner;
 import java.util.Stack;
 
 public class Pilha {
 
 	public static void main(String[] args) {
 		
+		Scanner leia = new Scanner(System.in);
+		
+		// Cria a Estrutura de dados Pilha
 		Stack<String> pilha = new Stack<String>();
 		
+		// Adiciona Elementos na Pilha
 		pilha.add("Comunicação não Violenta");
 		pilha.add("IT: A Coisa");
 		pilha.add("A Coragem de ser imperfeito");
@@ -16,18 +21,25 @@ public class Pilha {
 		pilha.add("Senhor dos Aneis");
 		pilha.add("O Codigo da Vini");
 		
+		//Adicionando elemento via tela na Pilha
+		System.out.println("Digite o Titulo de um livro: ");
+		pilha.push(leia.nextLine());
+		
 		System.out.println(pilha);
 		
+		// Retira um elemento da Pilha
 		pilha.pop();
 		
 		System.out.println(pilha);
 		
+		// Adiciona um Elemento na Pilha
 		pilha.push("Roube como um Artista");
 		
 		System.out.println(pilha);
-		
+		// Exibe o elemento que esta no topo da pilha
 		System.out.println(pilha.peek());
 		
+		// Verifica se um elemento existe na pilha
 		System.out.println("Diario de um Banana, existe na Pilha: " + pilha.contains("Diario de um Banana"));
 		
 	}
